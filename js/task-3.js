f7cb2c73f2459661986c5cf89a9a5e16c8f058d3
taras <t.chaikivskyi@gmail.com>
2025-08-22 01:27:42 +0200

    implement all tasks

--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,17 @@
-function getElementWidth(content, padding, border) {
-  const contentWidth = parseFloat(content);
-  const contentPadding = parseFloat(padding);
-  const contentBorder = parseFloat(border);
+function checkForSpam(message) {
+  if (message.toLowerCase().includes('spam') || message.toLowerCase().includes('sale')) {
+    return true;
+  }
 
-  return contentWidth + contentPadding * 2 + contentBorder * 2;
+  return false;
 }
 
-console.group('TASK N3');
-console.log(getElementWidth('50px', '8px', '4px')); // 74
-console.log(getElementWidth('60px', '12px', '8.5px')); // 101
-console.log(getElementWidth('200px', '0px', '0px')); // 200
+console.groupCollapsed('TASK N3');
+console.log(checkForSpam('Latest technology news')); // false
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+console.log(checkForSpam('Get best sale offers now!')); // true
+console.log(checkForSpam('Amazing SalE, only tonight!')); // true
+console.log(checkForSpam('Trust me, this is not a spam message')); // true
+console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
 console.groupEnd();
